b667cd7ae849b0525dfe829de3b70af58bf85d9f
NourelhoudaOKMID <nour.okmid@gmail.com>
2026-01-15 09:31:27 +0100

    fix add the logic of the calculator

--- public/Js/app.js
+++ public/Js/app.js
@@ -1,17 +1,81 @@
-const display = document.getElementById('display');
-const buttons = document.querySelectorAll('button');
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll(".buttons button");
 
-let currentInput = '';
+let current = "";
+let previous = "";
+let operator = null;
 
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+// to display the numbers in the input with control the curr numbers to be visible 
+    if (button.dataset.number) {
+      current += button.dataset.number;
+      display.value = current;
+    }
+// btns that have = +...
+    if (button.dataset.operator) {
+      if (current === "") return;
 
+      if (previous !== "") {
+        calculate();
+      }
 
-buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        const value = btn.textContent
-        if (value === "C") {
-            currentInput = currentInput.slice(0,-1)
-            display.value = currentInput || "0"
+      operator = button.dataset.operator;
+      previous = current;
+      current = "";
+    }
 
-        }
-    })
-})
+    if (button.dataset.action === "equals") {
+      if (current === "" || previous === "") return;
+      calculate();
+      operator = null;
+    }
+// clear one numb
+    if (button.dataset.action === "clear") {
+      current = current.slice(0, -1);
+      display.value = current || "0";
+    }
+//clear all
+    if (button.dataset.action === "all-clear") {
+      current = "";
+      previous = "";
+      operator = null;
+      display.value = "0";
+    }
+//todo: fix this and search for another method
+    if (button.dataset.action === "percent") {
+      current = (parseFloat(current) / 100).toString();
+      display.value = current;
+    }
+  });
+});
+
+function calculate() {
+  let result;
+
+  const prev = parseFloat(previous);
+  const curr = parseFloat(current);
+// check if is not a numb
+  if (isNaN(prev) || isNaN(curr)) return;
+
+  switch (operator) {
+    case "+":
+      result = prev + curr;
+      break;
+    case "-":
+      result = prev - curr;
+      break;
+    case "*":
+      result = prev * curr;
+      break;
+    case "/":
+      result = curr === 0 ? "Error" : prev / curr;
+      break;
+    default:
+      return;
+  }
+
+  current = result.toString();
+  previous = "";
+  display.value = current;
+}
